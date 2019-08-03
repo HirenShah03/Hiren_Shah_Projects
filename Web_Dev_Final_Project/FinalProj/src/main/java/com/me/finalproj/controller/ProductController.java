321d26c98b4ef776dd55fd346ce8a59a0bf9fd66
HirenShah03 <31998764+HirenShah03@users.noreply.github.com>
2019-08-03 13:13:55 -0400

    Update ProductController.java

--- Web_Dev_Final_Project/FinalProj/src/main/java/com/me/finalproj/controller/ProductController.java
+++ Web_Dev_Final_Project/FinalProj/src/main/java/com/me/finalproj/controller/ProductController.java
@@ -53,19 +53,19 @@ public class ProductController {
 	@RequestMapping(value = "/add/products", method = RequestMethod.POST)
 	protected ModelAndView addHousing(HttpServletRequest request,@RequestParam("imagepaths") MultipartFile file,@ModelAttribute("product") Product product) throws Exception {
 		if(validateSession(request)) {
-    	HttpSession session = (HttpSession) request.getSession();
-		Set<Category> categories = new HashSet<Category>();
-		String uploadPath=context.getRealPath(File.separator)+"\\resources\\data\\";
-		String title=request.getParameter("title");
-		String description=request.getParameter("description");
-		String[] categoriesArray=product.getCategoryArray();
-		int quotation=Integer.parseInt(request.getParameter("quotations").substring(1));
-		
-		
-		for(String c:categoriesArray) {
-			Category category;
-			category=categoryDAO.get(c);
-			categories.add(category);
+			HttpSession session = (HttpSession) request.getSession();
+			Set<Category> categories = new HashSet<Category>();
+			String uploadPath=context.getRealPath(File.separator)+"\\resources\\data\\";
+			String title=request.getParameter("title");
+			String description=request.getParameter("description");
+			String[] categoriesArray=product.getCategoryArray();
+			int quotation=Integer.parseInt(request.getParameter("quotations").substring(1));
+
+
+			for(String c:categoriesArray) {
+				Category category;
+				category=categoryDAO.get(c);
+				categories.add(category);
 		}
 		product.setTitle(title);
 		product.setDescription(description);
@@ -74,30 +74,28 @@ public class ProductController {
 		product.setUser((User)session.getAttribute("user"));
 		product.setCategories(categories);
 		
-        FileCopyUtils.copy(file.getBytes(), new File(uploadPath+file.getOriginalFilename()));
-        productDao.addProduct(product);
+        	FileCopyUtils.copy(file.getBytes(), new File(uploadPath+file.getOriginalFilename()));
+     		productDao.addProduct(product);
 
 		for(Category c: product.getCategories()){
-        	c = categoryDAO.get(c.getName());
-        	c.getProducts().add(product);
-        	categoryDAO.update(c);
-        }
+			c = categoryDAO.get(c.getName());
+			c.getProducts().add(product);
+			categoryDAO.update(c);
+      		  }
 		return new ModelAndView("addpost","success","Product Successfully Posted!!");
 		
-	}else {
-		return new ModelAndView("home","errorMessage","Your Session Expired, kindly try to login again");
-		
-	}
+	       }else {
+		return new ModelAndView("home","errorMessage","Your Session Expired, kindly try to login again");	
+		}
 	}
 	@RequestMapping(value = "/search/products", method = RequestMethod.GET)
 	protected ModelAndView searchHousing(HttpServletRequest request) throws Exception {
-		if(validateSession(request)) {
+	   if(validateSession(request)) {
 		request.setAttribute("product", product);
 		return new ModelAndView("searchProduct","categories", categoryDAO.list());
-	}else {
+	  }else {
 		return new ModelAndView("home","errorMessage","Your Session Expired, kindly try to login again");
-		
-	 }
+	       }
 	}
 	
 	@RequestMapping(value="/search/products/ajaxload", method=RequestMethod.GET)
@@ -110,7 +108,6 @@ public class ProductController {
 		try {
 			List<Product> productList=productDao.getApproved();
 			 JSONArray jArray = null;
-//				housingJson = new Gson().toJson(productList);
 				JSONObject jObject = new JSONObject();
 				try
 				{
@@ -128,12 +125,11 @@ public class ProductController {
 				} catch (JSONException jse) {
 				}
 
-				System.out.println(jArray);
 				productJson=jArray.toString();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		  }
 		}
 		return productJson;
 	}
@@ -170,7 +166,6 @@ public class ProductController {
 				System.out.println(jArray);
 				productJson=jArray.toString();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		}
@@ -180,9 +175,9 @@ public class ProductController {
 	protected String validateHousing(HttpServletRequest request) throws Exception {
 		if(validateSession(request)) {
 
-		return "productAdmin";
+		   return "productAdmin";
 		}else
-			return null;
+		   return null;
 	}
 
 	@RequestMapping(value="/validate/products/ajaxload", method=RequestMethod.GET)
@@ -192,33 +187,32 @@ public class ProductController {
 		String housingJson=null;
 		if(validateSession(request)) {
 
-		try {
-			List<Product> productList=productDao.getPending();
-			 JSONArray jArray = null;
-//			housingJson = new Gson().toJson(productList);
-			JSONObject jObject = new JSONObject();
-			try
-			{
-			    jArray = new JSONArray();
-			    for (Product product : productList)
-			    {
-			         JSONObject studentJSON = new JSONObject();
-			         studentJSON.put("id", product.getId());
-			         studentJSON.put("title", product.getTitle());
-			         studentJSON.put("imagepath", product.getImagepath());
-			         studentJSON.put("quotation", product.getQuotation());
-			         jArray.put(studentJSON);
-			    }
-			    jObject.put("StudentList", jArray);
-			} catch (JSONException jse) {
-			}
+			try {
+				List<Product> productList=productDao.getPending();
+				 JSONArray jArray = null;
+				JSONObject jObject = new JSONObject();
+				try
+				{
+				    jArray = new JSONArray();
+				    for (Product product : productList)
+				    {
+					 JSONObject studentJSON = new JSONObject();
+					 studentJSON.put("id", product.getId());
+					 studentJSON.put("title", product.getTitle());
+					 studentJSON.put("imagepath", product.getImagepath());
+					 studentJSON.put("quotation", product.getQuotation());
+					 jArray.put(studentJSON);
+				    }
+				    jObject.put("StudentList", jArray);
+				} catch (JSONException jse) {
+				}
 
-			System.out.println(jArray);
-			housingJson=jArray.toString();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+				System.out.println(jArray);
+				housingJson=jArray.toString();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return housingJson;
 	}
@@ -230,21 +224,20 @@ public class ProductController {
 	{
 		String result = null;
 		if(validateSession(request)) {
-		long id=Long.parseLong(request.getParameter("id"));
-		String decision=request.getParameter("decision");
-//		System.out.println("here"+request.getParameter("id"));
-		try {
-			 boolean flag=productDao.updateRequest(id,decision);
-			 if(flag) {
-				 result="done";
-			 }
-			 else {
-				 result="error";
-			 }
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			long id=Long.parseLong(request.getParameter("id"));
+			String decision=request.getParameter("decision");
+			try {
+				 boolean flag=productDao.updateRequest(id,decision);
+				 if(flag) {
+					 result="done";
+				 }
+				 else {
+					 result="error";
+				 }
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return result;
 	}
@@ -255,32 +248,31 @@ public class ProductController {
 	{
 		String categoryJson=null;
 		if(validateSession(request)) {
-		try {
-			List<Category> catList=categoryDAO.list();
-			 JSONArray jArray = null;
-//				housingJson = new Gson().toJson(productList);
-				JSONObject jObject = new JSONObject();
-				try
-				{
-				    jArray = new JSONArray();
-				    for (Category category : catList)
-				    {
-				         JSONObject studentJSON = new JSONObject();
-				         studentJSON.put("categoryId", category.getCategoryId());
-				         studentJSON.put("name", category.getName());
-				         studentJSON.put("products", category.getProducts());
-				         jArray.put(studentJSON);
-				    }
-				    jObject.put("StudentList", jArray);
-				} catch (JSONException jse) {
-				}
+			try {
+				List<Category> catList=categoryDAO.list();
+				 JSONArray jArray = null;
+					JSONObject jObject = new JSONObject();
+					try
+					{
+					    jArray = new JSONArray();
+					    for (Category category : catList)
+					    {
+						 JSONObject studentJSON = new JSONObject();
+						 studentJSON.put("categoryId", category.getCategoryId());
+						 studentJSON.put("name", category.getName());
+						 studentJSON.put("products", category.getProducts());
+						 jArray.put(studentJSON);
+					    }
+					    jObject.put("StudentList", jArray);
+					} catch (JSONException jse) {
+					}
 
-				System.out.println(jArray);
-				categoryJson=jArray.toString();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+					System.out.println(jArray);
+					categoryJson=jArray.toString();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return categoryJson;
 	}
@@ -290,7 +282,7 @@ public class ProductController {
 	public String filterContents(HttpServletRequest request,@RequestParam(value="clickedItem[]") String[] clickedItem)
 	{
 		String productJson=null;
-		if(validateSession(request)) {
+	  if(validateSession(request)) {
 		Long[] clicked=new Long[clickedItem.length];
 		for (int i = 0; i < clicked.length; i++){
 			clicked[i] = Long.parseLong(clickedItem[i]); 
@@ -326,20 +318,20 @@ public class ProductController {
 
 				System.out.println(jArray);
 				productJson=jArray.toString();
-		} catch (Exception e) {
+		  } catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		}
+	     }
 		return productJson;
 	}
 	public static  boolean validateSession(HttpServletRequest request) {
 		HttpSession session = (HttpSession) request.getSession();
 
 		if(!(session.getAttribute("user")==null)) {
-			return true;
+	 	     return true;
 		}
 		else
-		return false;
+		    return false;
 	}
 }
